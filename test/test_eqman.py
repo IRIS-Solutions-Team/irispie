e4397a458d24e7871ba305559eb05b1e6771f0e9
Jaromir Benes <jaromir.benes@gmail.com>
2022-10-08 15:14:52 +0200

    Added test folder

---
+++ test/test_eqman.py
@@ -0,0 +1,27 @@
+
+
+# import sys, importlib  as il
+# sys.path.append('..')
+# 
+# import modiphy.eqman as eq
+# il.reload(eq)
+
+eqs = list()
+
+for _ in range(1000):
+    eqs.extend([
+        eq.Equation(1, "a + b{-1} = c{+2} - 0.5*a!!a + b"),
+        eq.Equation(2, "z - b{+1} = 2*a"),
+    ])
+
+
+names = eq.names_from_equations(eqs)
+
+name_to_id = { name: id for id, name in enumerate(names) }
+
+xs, inc, *_ = eq.xtrings_from_equations(eqs, name_to_id)
+
+
+xsd =  [ i.split("!!")[-1] for i in xs ]
+xss =  [ i.split("!!")[0] for i in xs ]
+
