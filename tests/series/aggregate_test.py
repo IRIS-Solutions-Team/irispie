b2330cb92249c9e676651b4ec8a7ee86d00bd4b0
Jaromír Beneš <jaromir.benes@gmail.com>
2024-05-29 08:57:06 +0500

    Tests

---
+++ tests/series/aggregate_test.py
@@ -0,0 +1,80 @@
+
+import pytest
+import calendar as ca
+import random as rn
+import numpy as np
+import irispie as ir
+
+rn.seed(0)
+
+
+daily_span = ir.dd(2020,1,1) >> ir.dd(2021,12,31)
+
+parameter_names = "method, result_func"
+
+parameters = [
+    ("sum", lambda num_days: (1 + num_days) * num_days / 2),
+    ("mean", lambda num_days: (1 + num_days) / 2),
+    (None, lambda num_days: (1 + num_days) / 2),
+    ("first", lambda num_days: 1),
+    ("last", lambda num_days: num_days),
+]
+
+# Daily to yearly
+
+montly_span = ir.mm(2020,1) >> ir.mm(2021,12)
+monthly_period_value = ((t, t.day) for t in daily_span)
+periods, values = zip(*monthly_period_value)
+DAILY_TO_MONTHLY_SERIES = ir.Series(periods=periods, values=values, )
+
+@pytest.mark.parametrize(parameter_names, parameters, )
+def test_daily_to_monthly(method, result_func, d=DAILY_TO_MONTHLY_SERIES, ):
+    m = ir.aggregate(d, ir.MONTHLY, method=method, )
+    for t in montly_span:
+        _, num_days = ca.monthrange(t.year, t.period)
+        assert m[t] == result_func(num_days, )
+
+
+# Daily to yearly
+
+
+def day_in_year(t):
+    return t.serial - ir.daily_serial_from_ymd(t.year, 1, 1) + 1
+yearly_span = ir.yy(2020) >> ir.yy(2021)
+yearly_period_value = ((t, day_in_year(t)) for t in daily_span)
+periods, values = zip(*yearly_period_value)
+DAILY_TO_YEARLY_SERIES = ir.Series(periods=periods, values=values, )
+
+
+@pytest.mark.parametrize(parameter_names, parameters, )
+def test_daily_to_yearly(method, result_func, d=DAILY_TO_YEARLY_SERIES, ):
+    m = ir.aggregate(d, ir.YEARLY, method=method, )
+    for t in yearly_span:
+        num_days = (ca.isleap(t.year) and 366) or 365
+        assert m[t] == result_func(num_days, )
+
+
+daily_span = ir.dd(2020,1,1) >> ir.dd(2021,12,31)
+montly_span = ir.mm(2020,1) >> ir.mm(2021,12)
+monthly_period_value = ((t, t.day if t.day != 15 else None) for t in daily_span)
+periods, values = zip(*monthly_period_value)
+DAILY_SERIES_MISSING = ir.Series(periods=periods, values=values, )
+
+
+parameter_names = "method, result_func"
+
+parameters = [
+    ("sum", lambda num_days: sum(range(1, num_days + 1)) - 15),
+    ("mean", lambda num_days: (sum(range(1, num_days + 1)) - 15) / (num_days - 1)),
+    (None, lambda num_days: (sum(range(1, num_days + 1)) - 15) / (num_days - 1)),
+    ("first", lambda num_days: 1),
+    ("last", lambda num_days: num_days),
+]
+
+@pytest.mark.parametrize(parameter_names, parameters, )
+def test_daily_to_monthly_missing(method, result_func, d=DAILY_SERIES_MISSING, ):
+    m = ir.aggregate(d, ir.MONTHLY, method=method, remove_missing=True, )
+    for t in montly_span:
+        _, num_days = ca.monthrange(t.year, t.period)
+        assert m[t] == result_func(num_days, )
+
--- tests/series/hpf_test.py
+++ tests/series/hpf_test.py
@@ -1,17 +1,51 @@
 
-import sys
-sys.path.append("../../../src")
+import pytest
+import random as rn
+import numpy as np
+import irispie as ir
 
-import random as _ra
+rn.seed(0)
 
-from irispie import *
 
-#x = Series.from_func(qq(2020,1)>>qq(2025,4), _ra.gauss, num_columns=2)
-x = 10+Series.from_func(qq(2020,1)>>qq(2025,4), _ra.gauss, )
-#x = exp(x)
+def sum_round(x, tolerance=10, ):
+    return np.round(x, tolerance).sum()
 
-start_date = qq(2020,1)
-end_date = qq(2025,4) + 8
-change = Series.from_start_date_and_values(end_date, 0)
-xt, xg = x.hpf(log=False, range=start_date>>end_date, )#change=change, )
+
+def random_series():
+    return ir.Series(periods=ir.qq(2020,1)>>ir.qq(2025,4), func=rn.gauss, )
+
+
+N = 10
+
+
+@pytest.mark.parametrize(
+    "x", [ random_series() for _ in range(N, ) ]
+)
+def test_hpf_additive(x, ):
+    xt, xg = ir.hpf(x, )
+    assert sum_round((xt + xg).get_data() - x.get_data()) == 0
+
+
+@pytest.mark.parametrize(
+    "x, level_value", [(random_series(), rn.uniform(0, 1)) for _ in range(N, )]
+)
+def test_hpf_level(x, level_value, ):
+    x = ir.Series(periods=ir.qq(2020,1)>>ir.qq(2025,4), func=rn.gauss, )
+    level_date = x.end
+    level = ir.Series(periods=level_date, values=level_value, )
+    xt, xg = ir.hpf(x, level=level, )
+    assert sum_round(xt[level_date] - level_value, 0) == 0
+
+
+@pytest.mark.parametrize(
+    "x, change_value", [(random_series(), rn.uniform(0, 1)) for _ in range(N, )]
+)
+def test_hpf_change(x, change_value, ):
+    x = ir.Series(periods=ir.qq(2020,1)>>ir.qq(2025,4), func=rn.gauss, )
+    change_date = x.end
+    change = ir.Series(periods=change_date, values=change_value, )
+    xt, xg = ir.hpf(x, change=change, )
+    diff_xt = ir.diff(xt)
+    assert sum_round(diff_xt[change_date] - change_value, 0) == 0
+
 
