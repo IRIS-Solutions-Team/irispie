bec0d57c8408caf390caeac462b560fd35841166
Jaromír Beneš <jaromir.benes@gmail.com>
2025-02-03 17:38:41 +0100

    Bugfixed missing directory

---
+++ src/irispie/series/_categories.py
@@ -0,0 +1,20 @@
+"""
+Documark categories for the Series class
+"""
+
+
+CATEGORIES = {
+    "constructor": "Constructing new time series",
+    "conversion": "Converting time series frequency",
+    "information": "Getting information about time series",
+    "manipulation": "Manipulating time series values",
+    "multiple": "Combining multiple time series",
+    "homogenizing": "Homogenizing and extrapolating time series",
+    "filtering": "Filtering time series",
+    "moving": "Applying moving window functions",
+    "temporal_change": "Calculating temporal change",
+    "temporal_change_conversion": "Converting measures of temporal change",
+    "temporal_cumulation": "Calculating temporal cumulation",
+    "indexing": "Indexing time series",
+}
+
